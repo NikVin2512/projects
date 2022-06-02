1beeb9f75c7d0cde8bc63e972b21aa6ae31eb20c
Nikolay Vinogradov <was.bic@gmail.com>
2022-06-02 23:10:38 +0300

    Ошибка

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-let int = 266219;
-let func = num => Number(num);
-let arr = Array.from(String(int), func);
+let title = "Lesson02";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 4;
+let rollback = 50;
+let fullPrice = 10000;
+let adaptive = true;
 
-console.log(arr);
-let res = arr.reduce((acc, rec) => acc * rec);
-
-console.log(res);
-let rez = res ** 3;
-console.log(rez);
-console.log(String(rez).slice(0,2));
+console.log(typeof title,typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice + "$");
+console.log(fullPrice + "$");
+console.log(screens.toLocaleLowerCase().split(", "));
+console.log(fullPrice * (rollback/100) + "$");
