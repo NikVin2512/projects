7bc3c3aa72534a7ac84c54d81cecad8b53462e96
Nikolay Vinogradov <was.bic@gmail.com>
2022-06-02 22:32:37 +0300

    Выполнено ДЗ Урок 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Lesson02";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 4;
+let rollback = 50;
+let fullPrice = 10000;
+let adaptive = true;
 
-alert("My First alert");
-console.log("My first log");
+console.log(typeof title,typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice + "$");
+console.log(fullPrice + "$");
+console.log(screens.toLocaleLowerCase().split(", "));
+console.log(fullPrice * (rollback/100) + "$");
